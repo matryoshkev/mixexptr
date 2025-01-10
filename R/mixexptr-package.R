31bbd64818363ad0e0de41f3acd12bb95edb9566
jeff smith <matryoshkev@gmail.com>
2025-01-10 10:46:09 -0600

    Add package help (e.g. ?mixexptr)

--- R/mixexptr-package.R
+++ R/mixexptr-package.R
@@ -1,3 +1,11 @@
+#' mixexptr: Analyze Microbial Mix Experiments
+#'
+#' Tools to analyze microbial interactions in mix experiments,
+#' which measure how fitness and behavior change with the local frequency of
+#' different microbe types. 'mixexptr' provides tools to calculate and
+#' visualize fitness measures that help researchers get the most out of their
+#' data and quantitatively compare results across systems.
+#'
 #' @keywords internal
 "_PACKAGE"
 
